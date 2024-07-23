fc7c11317d011966d70b2199ba397199294bec8d
vinsmake <ps.enrique.e@gmail.com>
2024-07-23 12:47:52 -0600

    added logos with link

--- wp-content/themes/armoniamatutina/front-page.php
+++ wp-content/themes/armoniamatutina/front-page.php
@@ -12,14 +12,53 @@ if ($titulo) {
 }
 
 /* SOCIALS */
+$discord = get_field('discord');
+if ($discord) {
+    update_option('custom_discord', $discord);
+}
+
 $facebook = get_field('facebook');
 if ($facebook) {
     update_option('custom_facebook', $facebook);
 }
+
 $instagram = get_field('instagram');
 if ($instagram) {
     update_option('custom_instagram', $instagram);
 }
+
+$linkedin = get_field('linkedin');
+if ($linkedin) {
+    update_option('custom_linkedin', $linkedin);
+}
+
+$telegram = get_field('telegram');
+if ($telegram) {
+    update_option('custom_telegram', $telegram);
+}
+
+$tiktok = get_field('tiktok');
+if ($tiktok) {
+    update_option('custom_tiktok', $tiktok);
+}
+
+$whatsapp = get_field('whatsapp');
+if ($whatsapp) {
+    update_option('custom_whatsapp', $whatsapp);
+}
+
+$x = get_field('x');
+if ($x) {
+    update_option('custom_x', $x);
+}
+
+$youtube = get_field('youtube');
+if ($youtube) {
+    update_option('custom_youtube', $youtube);
+}
+
+
+
 ?>
 
 
--- wp-content/themes/armoniamatutina/header.php
+++ wp-content/themes/armoniamatutina/header.php
@@ -3,8 +3,16 @@
 $leyenda = get_option('custom_leyenda');
 $titulo = get_option('custom_titulo');
 /* SOCIALS */
+$discord = get_option('custom_discord');
 $facebook = get_option('custom_facebook');
 $instagram = get_option('custom_instagram');
+$linkedin = get_option('custom_linkedin');
+$telegram = get_option('custom_telegram');
+$tiktok = get_option('custom_tiktok');
+$whatsapp = get_option('custom_whatsapp');
+$x = get_option('custom_x');
+$youtube = get_option('custom_youtube');
+
 ?>
 
 
@@ -49,45 +57,179 @@ $instagram = get_option('custom_instagram');
 
                 </h2>
                 <!-- Social media -->
-                 <div>
-                <?php
-                /* Facebook */
-                if ($facebook) {
-                    if ($facebook['mostrar']) {
-                        $link = esc_url($facebook['link']);
-                        $svg_path = get_template_directory() . '/svg/facebook.svg';
+                <div class="header__hero__contenido__social">
+                    <?php
+                    /* Discord */
+                    if ($discord) {
+                        if ($discord['mostrar']) {
+                            $link = esc_url($discord['link']);
+                            $svg_path = get_template_directory() . '/svg/discord.svg';
 
-                        if (file_exists($svg_path)) {
-                            $svg = file_get_contents($svg_path);
-                            echo '<a href="' . $link . '">' . $svg . '</a>';
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
                         } else {
                             echo '';
                         }
                     } else {
                         echo '';
                     }
-                } else {
-                    echo '';
-                }
-                /* Instagram */
-                if ($instagram) {
-                    if ($instagram['mostrar']) {
-                        $link = esc_url($instagram['link']);
-                        $svg_path = get_template_directory() . '/svg/instagram.svg';
+                    /* Facebook */
+                    if ($facebook) {
+                        if ($facebook['mostrar']) {
+                            $link = esc_url($facebook['link']);
+                            $svg_path = get_template_directory() . '/svg/facebook.svg';
 
-                        if (file_exists($svg_path)) {
-                            $svg = file_get_contents($svg_path);
-                            echo '<a href="' . $link . '">' . $svg . '</a>';
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
                         } else {
                             echo '';
                         }
                     } else {
                         echo '';
                     }
-                } else {
-                    echo '';
-                }
-                ?>
+                    /* Instagram */
+                    if ($instagram) {
+                        if ($instagram['mostrar']) {
+                            $link = esc_url($instagram['link']);
+                            $svg_path = get_template_directory() . '/svg/instagram.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* LinkedIn */
+                    if ($linkedin) {
+                        if ($linkedin['mostrar']) {
+                            $link = esc_url($linkedin['link']);
+                            $svg_path = get_template_directory() . '/svg/linkedin.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* Telegram */
+                    if ($telegram) {
+                        if ($telegram['mostrar']) {
+                            $link = esc_url($telegram['link']);
+                            $svg_path = get_template_directory() . '/svg/telegram.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* TikTok */
+                    if ($tiktok) {
+                        if ($tiktok['mostrar']) {
+                            $link = esc_url($tiktok['link']);
+                            $svg_path = get_template_directory() . '/svg/tiktok.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* WhatsApp */
+                    if ($whatsapp) {
+                        if ($whatsapp['mostrar']) {
+                            $link = esc_url($whatsapp['link']);
+                            $svg_path = get_template_directory() . '/svg/whatsapp.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* X */
+                    if ($x) {
+                        if ($x['mostrar']) {
+                            $link = esc_url($x['link']);
+                            $svg_path = get_template_directory() . '/svg/x.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+                    /* YouTube */
+                    if ($youtube) {
+                        if ($youtube['mostrar']) {
+                            $link = esc_url($youtube['link']);
+                            $svg_path = get_template_directory() . '/svg/youtube.svg';
+
+                            if (file_exists($svg_path)) {
+                                $svg = file_get_contents($svg_path);
+                                echo '<a href="' . $link . '" target="_blank">' . $svg . '</a>';
+                            } else {
+                                echo '';
+                            }
+                        } else {
+                            echo '';
+                        }
+                    } else {
+                        echo '';
+                    }
+
+
+                    ?>
                 </div>
             </div>
         </section>
