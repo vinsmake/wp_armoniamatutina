c6976432f53b14962a22b56b0ea96d09b1e2102e
vinsmake <ps.enrique.e@gmail.com>
2024-08-06 10:27:39 -0600

    navbar script working

--- wp-content/themes/armoniamatutina/js/scripts.js
+++ wp-content/themes/armoniamatutina/js/scripts.js
@@ -1,4 +1,16 @@
 function armonia_matutina_script() {
     console.log('scripts working');
+
+    /* hamburguer menu */
+    const hamburguer = document.querySelector('.hamburger');
+    hamburguer.addEventListener('click', function () {
+        const menuPrincipal = document.querySelector('.header__navbar__active');
+        if (menuPrincipal.style.transform === 'translateX(101%)' || menuPrincipal.style.transform === '') {
+            menuPrincipal.style.transform = 'translateX(0%)';
+        } else {
+            menuPrincipal.style.transform = 'translateX(101%)';
+        }
+    });
 }
-document.addEventListener('DOMContentLoaded', armonia_matutina_script)
+
+document.addEventListener('DOMContentLoaded', armonia_matutina_script);
